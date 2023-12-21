562f0126d8e249bf51c0fc3e74af932a4e28bd2a
SZTekk <Saeztech@gmail.com>
2023-12-21 21:50:26 +0500

    socialMedia test

---
+++ cypress/e2e/tests/socialMediaIcons.cy.js
@@ -0,0 +1,22 @@
+import { SocialMediaIcons } from "../../pages/socialMediaIcons";
+const socialMediaIcons = new SocialMediaIcons;
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
+
+describe("validate user can visit Verizon social media", () =>{
+    let testdata;
+    before(function () {
+        cy.visit(Cypress.env("url"))
+        cy.fixture('data.json').then((data) => {
+            testdata = data
+        })
+    })
+
+    it("validate user can visit different Verizon social media handles", async () => {
+        await socialMediaIcons.validateSocialMediaIcons()
+    })
+})
---
+++ cypress/pages/socialMediaIcons.js
@@ -0,0 +1,18 @@
+export class SocialMediaIcons {
+
+elements = {
+    followVerizonFooter: () => cy.get('[aria-label="Follow Verizon menu list"]'),
+    facebookButton: () =>cy.get('[aria-label="Follow Verizon on Facebook"]'),
+    twitterButton: () =>cy.get('[aria-label="Follow Verizon on Twitter"]'),
+    youtubeButton: () =>cy.get('[aria-label="Follow Verizon on Youtube"]'),
+    instagramButton: () =>cy.get('[aria-label="Follow Verizon on Instagram"]'),
+}
+
+    validateSocialMediaIcons = () => {
+        this.elements.followVerizonFooter().scrollIntoView();
+        this.elements.facebookButton().eq(0).should('have.attr', 'href').and('include', 'facebook.com');
+        this.elements.twitterButton().eq(0).should('have.attr', 'href').and('include', 'twitter.com');
+        this.elements.youtubeButton().eq(0).should('have.attr', 'href').and('include', 'youtube.com');
+        this.elements.instagramButton().eq(0).should('have.attr', 'href').and('include', 'instagram.com');
+    }
+}
